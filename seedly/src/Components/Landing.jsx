ac67c36ea3af68829e267e0a12d49a25242c758c
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-28 14:45:42 -0400

    added styling to landing and added button

--- seedly/src/Components/Landing.jsx
+++ seedly/src/Components/Landing.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export default class Landing extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="LandingPage">
+                <br/>
+                <h1 style={{fontFamily: "Pacifico", fontSize: "200pt", color: "white"}}>Seedly</h1>
+                <br/> <br/>
+
+                <button>Get Started</button>
             </div>
         )
     }
