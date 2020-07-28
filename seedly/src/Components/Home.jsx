bb8f4241b3e1284f59124955bd4a6c4eb599e5ca
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-28 15:36:33 -0400

    editing homepage header

--- seedly/src/Components/Home.jsx
+++ seedly/src/Components/Home.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Router>
+                <div className="Homepage">
+                    <div className="Logo">
+                        <h1 style={{fontFamily: "Pacifico", fontSize: "35pt", color: "black", position: "relative", right: "42vw"}}>Seedly</h1>
+                    </div>
+                    <div className="Navbar">
+                        <h2 style={{fontFamily: "Pacifico", fontSize: "20pt", color: "#000000"}}>My Plants</h2>
+                        <h2 style={{fontFamily: "Pacifico", fontSize: "20pt", color: "#000000"}}>My Plots</h2>
+                    </div>
+                </div>
+            </Router>
         )
     }
 }
