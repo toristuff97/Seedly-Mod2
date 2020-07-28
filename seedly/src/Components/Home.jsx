a2b278da68da84097c2e71942afbc63142adc88c
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-28 15:42:36 -0400

    added styling to Home navbar

--- seedly/src/Components/Home.jsx
+++ seedly/src/Components/Home.jsx
@@ -7,12 +7,14 @@ export default class Home extends Component {
         return (
             <Router>
                 <div className="Homepage">
+                    <br/>
                     <div className="Logo">
                         <h1 style={{fontFamily: "Pacifico", fontSize: "35pt", color: "black", position: "relative", right: "42vw"}}>Seedly</h1>
                     </div>
                     <div className="Navbar">
-                        <h2 style={{fontFamily: "Pacifico", fontSize: "20pt", color: "#000000"}}>My Plants</h2>
-                        <h2 style={{fontFamily: "Pacifico", fontSize: "20pt", color: "#000000"}}>My Plots</h2>
+                        <h2 style={{fontFamily: "Pacifico", fontSize: "20pt", color: "#000000"}}>My Plants&nbsp; &nbsp;</h2>
+                        <h2 style={{fontFamily: "Pacifico", fontSize: "20pt", color: "#000000"}}>My Plots&nbsp; &nbsp;</h2>
+                        <h2 style={{fontFamily: "Pacifico", fontSize: "20pt", color: "#000000"}}>Calendar&nbsp; &nbsp;</h2>
                     </div>
                 </div>
             </Router>
