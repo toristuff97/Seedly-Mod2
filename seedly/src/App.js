04a2a3bd2dc1822a6a4e0b0728b88dc09e83ec13
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-28 15:02:32 -0400

    updated react-router to react-router-dom

--- seedly/src/App.js
+++ seedly/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Landing from './Components/Landing'
+import Home from './Components/Home'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Landing />
+      <Route exact path="/">
+        <Landing />
+      </Route>
+      <Route exact path="/Home">
+        <Home />
+      </Route>
     </div>
+    </Router>
   );
 }
 
 export default App;
+
--- seedly/src/Components/Landing.jsx
+++ seedly/src/Components/Landing.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Router} from 'react-router'
+import {Router} from 'react-router-dom'
 
 export default class Landing extends Component {
     render() {
