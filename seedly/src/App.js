a514c4b3270476434cd29f9dbc544d8b70853802
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-29 09:10:43 -0400

    removed extra explore route

--- seedly/src/App.js
+++ seedly/src/App.js
@@ -28,9 +28,6 @@ function App() {
           <Route path="/MyPlants">
             <MyPlants />
           </Route>
-          <Route path="/Explore">
-            <MyPlants />
-          </Route>
         </div>
         </Switch>
     </Router>
