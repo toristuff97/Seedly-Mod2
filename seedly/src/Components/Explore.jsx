a51032e8ee441f21c53410645517abfe1dcf468e
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-29 14:40:02 -0400

    api is rendering!

--- seedly/src/Components/Explore.jsx
+++ seedly/src/Components/Explore.jsx
@@ -20,7 +20,7 @@ class Explore extends Component {
                 try {
                     const res = await axios.get(BASE_URL + 'plants?token=' + TOKEN + '&filter_not[description]');
                     console.log(res.data);
-                    this.setState({plantInfo: res.data.data});
+                    this.setState({info: res.data.data});
                 } catch(err) {
                     console.error(err.message);
                 }
@@ -109,10 +109,11 @@ class Explore extends Component {
                 {
                     this.state.info.map(plant => {
                         console.log(plant);
+                        console.log(plant.common_name);
                         return(
                             <div>
-                                <h4 style={{color: "black", fontFamily: "Pacifico", fontSize:"10pt"}}>Name: {plant.data.common_name}</h4>
-                                <img src={plant.data.image_url} alt="Plant" />
+                                <h4 style={{color: "black", fontFamily: "Pacifico", fontSize:"10pt"}}>Name: {plant.common_name}</h4>
+                                <img src={plant.image_url} alt="Plant" />
                             </div>
                         )
                     })
