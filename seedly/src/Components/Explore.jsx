8ba5f157be557a8ba7f659e6beb3bd6482aa4aae
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-29 09:22:05 -0400

    added filters to explore

--- seedly/src/Components/Explore.jsx
+++ seedly/src/Components/Explore.jsx
@@ -7,7 +7,28 @@ class Explore extends Component {
         return (
             <div className="ExplorePage">
                 <div className="Filter">
-
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Vegetables</button> <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Fruits</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Herbs</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Flowers</button>
+                    <br/>
+                    <h3 style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000"}}>Sort By:</h3>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>A-Z</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Z-A</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Planting Season</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Harvest Season</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Time to Harvest</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Average Yield</button>
+                    <br/>
+                    <button style={{fontFamily: "Pacifico", fontSize: "15pt", color: "#000000", border: "none", backgroundColor: "white"}}>Space Required</button>
+                    <br/>
                 </div>
                 <div className="Results">
 
