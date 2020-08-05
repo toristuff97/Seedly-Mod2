66ffdbb92bd64026d20d5aa7fd1a32546871fb9b
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-08-04 21:37:01 -0400

    added comment

--- seedly/src/Components/Result.jsx
+++ seedly/src/Components/Result.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap';
-
+// I planned for this component to be the display component for each result on the NewPlant page, but I don't think it does what I wanted it to do.
 const Result = (props) => {
     return (
         <div className="plant-result">
